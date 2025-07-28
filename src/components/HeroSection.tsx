dc3dc94b2de5dc92b75fdfd93d10fa6edd052534
RahymCharyyev <r569837@gmail.com>
2025-07-28 15:31:04 +0500

    ✨feature: Added download buttons at hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,28 +8,33 @@ const HeroSection = () => {
       id='home'
       className='min-h-screen flex items-center relative bg-gradient-to-br from-[#3D7EF9] to-[#2B529B] pt-24'
     >
-      <div className='max-w-6xl mx-auto px-4 grid md:grid-cols-2 gap-16 items-center z-10'>
+      <div className='max-w-6xl mx-auto px-4 grid md:grid-cols-2 items-center z-10'>
         <div className='text-white'>
           <h1 className='text-4xl md:text-5xl font-bold mb-4'>
             {t('hero.title')}
           </h1>
           <p className='text-lg mb-6'>{t('hero.description')}</p>
-          <div className='flex flex-wrap gap-4'>
+          <div className='flex gap-2'>
             <a
-              href='#download'
-              className='bg-gradient-to-r from-red-400 to-orange-500 text-white px-6 py-3 rounded-full font-bold shadow-lg hover:-translate-y-1 transition'
+              href='/tm-cargo.apk'
+              target='_blank'
+              download
+              className='flex gap-2 items-center bg-gradient-to-r from-red-400 to-orange-500 text-white px-6 py-3 rounded-full font-bold shadow-lg hover:-translate-y-1 transition'
             >
-              📱 {t('hero.download')}
+              <img className='w-6' src='/android.png' alt='Android icon' />{' '}
+              {t('hero.downloadAndroid')}
             </a>
             <a
-              href='#features'
-              className='border border-white/40 text-white px-6 py-3 rounded-full backdrop-blur hover:bg-white/20 transition'
+              href='https://apps.apple.com/tm/app/tmcargo/id6748551361'
+              target='_blank'
+              className='flex gap-2 items-center bg-gradient-to-r from-red-400 to-orange-500 text-white px-6 py-3 rounded-full font-bold shadow-lg hover:-translate-y-1 transition'
             >
-              🔍 {t('hero.learnMore')}
-            </a>
+              <img className='w-6' src='/apple.png' alt='ios icon' />{' '}
+              {t('hero.downloadIOS')}
+            </a>{' '}
           </div>
         </div>
-        <img src='/phone.png' alt='phone' className='w-7xl animate-float p-6' />
+        <img src='/phone.png' alt='phone' className='w-2xl animate-float p-6' />
       </div>
     </section>
   );
